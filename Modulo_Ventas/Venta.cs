79a7f243595178bced1220008ef210a5aa7ce5f4
OscarAR <oscarandresrolonmeneses@gmail.com>
2021-08-10 15:42:01 -0500

    venta

---
+++ Modulo_Ventas/Venta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller_Poo.Modulo_Ventas
+{
+    class Venta
+    {
+    }
+}
